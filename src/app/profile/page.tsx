63fca6af6d915c79b2982fbb883cd18b8a5de52a
navamirama <navami.ramamurthy@gmail.com>
2025-06-12 22:43:07 +0100

    Updated profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -96,6 +96,7 @@
 
 // export default ProfilePage;
 
+// @ts-nocheck
 import UpdateButton from "@/components/UpdateButton";
 import { updateUser } from "@/lib/actions";
 import { wixClientServer } from "@/lib/wixClientServer";
@@ -114,13 +115,13 @@ const ProfilePage = async () => {
     return <div className="">Not logged in!</div>;
   }
 
-  // const orderRes = await wixClient.orders.searchOrders(
-  //   {
-  //     search: {
-  //       filter: { "buyerInfo.contactId": { $eq: user.member?.contactId } },
-  //     },
-  //   }
-  // );
+  const orderRes = await wixClient.orders.searchOrders(
+    {
+      search: {
+        filter: { "buyerInfo.contactId": { $eq: user.member?.contactId } },
+      },
+    }
+  );
 
   return (
     <div className="flex flex-col md:flex-row gap-24 md:h-[calc(100vh-180px)] items-center px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
@@ -173,7 +174,7 @@ const ProfilePage = async () => {
       <div className="w-full md:w-1/2">
         <h1 className="text-2xl">Orders</h1>
         <div className="mt-12 flex flex-col">
-          {/* {orderRes.orders.map((order) => (
+          ` {orderRes.orders.map((order) => (
             <Link
               href={`/orders/${order._id}`}
               key={order._id}
@@ -188,7 +189,7 @@ const ProfilePage = async () => {
               )}
               <span className="w-1/4">{order.status}</span>
             </Link>
-          ))} */}
+          ))}`
         </div>
       </div>
     </div>
