a5537f2a7820be04fd7def8004132e85deef24be
navamirama <navami.ramamurthy@gmail.com>
2025-06-12 14:11:29 +0100

    Removed type strict

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -1,3 +1,158 @@
+// "use client";
+
+// import Image from "next/image";
+// import { useCartStore } from "@/hooks/useCartStore";
+// import { media as wixMedia } from "@wix/sdk";
+// import { useWixClient } from "@/hooks/useWixClient";
+// import { currentCart } from "@wix/ecom";
+// import { useEffect } from "react";
+
+// const CartModal = () => {
+//   // TEMPORARY
+//   // const cartItems = true;
+
+//   const wixClient = useWixClient();
+//   const { cart, getCart, isLoading, removeItem } = useCartStore();
+
+//   // useEffect(() => {
+//   //   getCart(wixClient);
+//   // }, [wixClient, getCart]);
+
+//   // console.log("cart", cart);
+
+//   const handleCheckout = async () => {
+//     console.log("Inside Checkout 1");
+//     try {
+//       const checkout =
+//         await wixClient.currentCart.createCheckoutFromCurrentCart({
+//           channelType: currentCart.ChannelType.WEB,
+//         });
+
+//       const { redirectSession } =
+//         await wixClient.redirects.createRedirectSession({
+//           ecomCheckout: { checkoutId: checkout.checkoutId },
+//           callbacks: {
+//             postFlowUrl: window.location.origin,
+//             thankYouPageUrl: `${window.location.origin}/success`,
+//           },
+//         });
+
+//       if (redirectSession?.fullUrl) {
+//         window.location.href = redirectSession.fullUrl;
+//       }
+//       console.log(cart);
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   };
+
+//   return (
+//     <div
+//       className="w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] top-12 right-0 flex flex-col gap-6 z-20"
+//       style={{ backgroundColor: "white" }}
+//     >
+//       {!cart.lineItems ? (
+//         <div className="">Cart is Empty</div>
+//       ) : (
+//         <>
+//           <h2 className="text-xl">Shopping Cart</h2>
+//           {/* LIST */}
+//           <div className="flex flex-col gap-8">
+//             {/* ITEM */}
+//             {cart.lineItems.map((item) => (
+//               <div className="flex gap-4" key={item._id}>
+//                 {item.image && (
+//                   <Image
+//                     src={wixMedia.getScaledToFillImageUrl(
+//                       item.image,
+//                       72,
+//                       96,
+//                       {}
+//                     )}
+//                     alt=""
+//                     width={72}
+//                     height={96}
+//                     className="object-cover rounded-md"
+//                   />
+//                 )}
+//                 <div className="flex flex-col justify-between w-full">
+//                   {/* TOP */}
+//                   <div className="">
+//                     {/* TITLE */}
+//                     <div className="flex items-center justify-between gap-8">
+//                       <h3 className="font-semibold">
+//                         {item.productName?.original}
+//                       </h3>
+//                       <div className="p-1 bg-gray-50 rounded-sm flex items-center gap-2">
+//                         {item.quantity && item.quantity > 1 && (
+//                           <div className="text-xs text-green-500">
+//                             {item.quantity} x{" "}
+//                           </div>
+//                         )}
+//                         ${item.price?.amount}
+//                       </div>
+//                     </div>
+//                     {/* DESC */}
+//                     <div className="text-sm text-gray-500">
+//                       {item.availability?.status}
+//                     </div>
+//                   </div>
+//                   {/* BOTTOM */}
+//                   <div className="flex justify-between text-sm">
+//                     <span className="text-gray-500">Qty. {item.quantity}</span>
+//                     <span
+//                       className="text-blue-500"
+//                       style={{
+//                         cursor: isLoading ? "not-allowed" : "pointer",
+//                         color: " blue",
+//                       }}
+//                       onClick={() => removeItem(wixClient, item._id!)}
+//                     >
+//                       Remove
+//                     </span>
+//                   </div>
+//                 </div>
+//               </div>
+//             ))}
+//           </div>
+//           {/* BOTTOM */}
+//           <div className="">
+//             <div className="flex items-center justify-between font-semibold">
+//               <span className="">Subtotal</span>
+//               <span className="">${cart.subtotal.amount}</span>
+//             </div>
+//             <p className="text-gray-500 text-sm mt-2 mb-4">
+//               Shipping and taxes calculated at checkout.
+//             </p>
+//             <div className="flex justify-between text-sm">
+//               <button
+//                 className="rounded-md py-3 px-4 ring-1 ring-gray-300"
+//                 style={{ backgroundColor: "black", color: "white" }}
+//               >
+//                 View Cart
+//               </button>
+//               <button
+//                 className="rounded-md py-3 px-4 disabled:cursor-not-allowed disabled:opacity-75"
+//                 style={{
+//                   backgroundColor: "black",
+//                   color: "white",
+//                   opacity: "75",
+//                 }}
+//                 disabled={isLoading}
+//                 onClick={handleCheckout}
+//               >
+//                 Checkout
+//               </button>
+//             </div>
+//           </div>
+//         </>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default CartModal;
+
 "use client";
 
 import Image from "next/image";
@@ -5,23 +160,15 @@ import { useCartStore } from "@/hooks/useCartStore";
 import { media as wixMedia } from "@wix/sdk";
 import { useWixClient } from "@/hooks/useWixClient";
 import { currentCart } from "@wix/ecom";
-import { useEffect } from "react";
 
 const CartModal = () => {
   // TEMPORARY
   // const cartItems = true;
 
   const wixClient = useWixClient();
-  const { cart, getCart, isLoading, removeItem } = useCartStore();
-
-  // useEffect(() => {
-  //   getCart(wixClient);
-  // }, [wixClient, getCart]);
-
-  // console.log("cart", cart);
+  const { cart, isLoading, removeItem } = useCartStore();
 
   const handleCheckout = async () => {
-    console.log("Inside Checkout 1");
     try {
       const checkout =
         await wixClient.currentCart.createCheckoutFromCurrentCart({
@@ -40,17 +187,13 @@ const CartModal = () => {
       if (redirectSession?.fullUrl) {
         window.location.href = redirectSession.fullUrl;
       }
-      console.log(cart);
     } catch (err) {
       console.log(err);
     }
   };
 
   return (
-    <div
-      className="w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] top-12 right-0 flex flex-col gap-6 z-20"
-      style={{ backgroundColor: "white" }}
-    >
+    <div className="w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
       {!cart.lineItems ? (
         <div className="">Cart is Empty</div>
       ) : (
@@ -102,10 +245,7 @@ const CartModal = () => {
                     <span className="text-gray-500">Qty. {item.quantity}</span>
                     <span
                       className="text-blue-500"
-                      style={{
-                        cursor: isLoading ? "not-allowed" : "pointer",
-                        color: " blue",
-                      }}
+                      style={{ cursor: isLoading ? "not-allowed" : "pointer" }}
                       onClick={() => removeItem(wixClient, item._id!)}
                     >
                       Remove
@@ -125,19 +265,11 @@ const CartModal = () => {
               Shipping and taxes calculated at checkout.
             </p>
             <div className="flex justify-between text-sm">
-              <button
-                className="rounded-md py-3 px-4 ring-1 ring-gray-300"
-                style={{ backgroundColor: "black", color: "white" }}
-              >
+              <button className="rounded-md py-3 px-4 ring-1 ring-gray-300">
                 View Cart
               </button>
               <button
-                className="rounded-md py-3 px-4 disabled:cursor-not-allowed disabled:opacity-75"
-                style={{
-                  backgroundColor: "black",
-                  color: "white",
-                  opacity: "75",
-                }}
+                className="rounded-md py-3 px-4 bg-black text-white disabled:cursor-not-allowed disabled:opacity-75"
                 disabled={isLoading}
                 onClick={handleCheckout}
               >
