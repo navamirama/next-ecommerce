78c5a8d7d177b8dd00deb8da9141eed24bcd1334
navamirama <navami.ramamurthy@gmail.com>
2025-12-18 13:12:58 +0000

    Enabled  pop up newsletter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ const HomePage = async () => {
   return (
     <div className="">
       {/* <Slider /> */}
-      {/* <PopupNewsletter /> */}
+      <PopupNewsletter />
       <div className="pt-8 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl bold">Featured Products</h1>
         <Suspense fallback={<Skeleton />}>
