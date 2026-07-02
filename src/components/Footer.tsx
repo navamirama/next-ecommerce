c1dd3957199f28814bcd07603822931192fa4efe
navamirama <navami.ramamurthy@gmail.com>
2026-07-02 18:42:59 +0100

    Added SLider 3 and 4 images

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,7 +17,7 @@ const Footer = () => {
         {/* LEFT */}
         <div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
           <Link href="/">
-            <div className="text-2xl tracking-wide">Vela Prints</div>
+            <div className="text-2xl tracking-wide">KULT Store</div>
           </Link>
           <p>
             England, United Kingdom
